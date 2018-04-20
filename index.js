e5582f05262f930a99b051b0ec6564e809ff33d1
david.goldstein <david.goldstein@deciphernow.com>
2018-04-20 14:05:55 -0400

    - added body parser

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.get('/', function(request, response) {
-  console.log(request)
+  console.log(JSON.stringify(request.body, null, 2))
   response.send('Hello World!')
 })
 
